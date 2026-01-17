1fa104ffeb54c8681035daf10aac9ce62c6e8f65
Gourav Prajapati <gouravprajapati7000@gmail.com>
2026-01-17 13:02:44 +0530

    Updated entities and repository

--- src/main/java/com/univ/repository/EmployeeRepository.java
+++ src/main/java/com/univ/repository/EmployeeRepository.java
@@ -1,5 +1,4 @@
 package com.univ.repository;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 import com.univ.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
